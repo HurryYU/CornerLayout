1e14f4afe0d8b6d63d5547438a06321ecb28069a
LinaPink <446682052@qq.com>
2020-09-17 10:35:47 +0800

    sample ues viewbinding

--- app/src/main/java/com/hurryyu/cornerlayout/sample/MainActivity.kt
+++ app/src/main/java/com/hurryyu/cornerlayout/sample/MainActivity.kt
@@ -2,12 +2,17 @@ package com.hurryyu.cornerlayout.sample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hurryyu.cornerlayout.sample.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        cornerLayout.bannerText = "HurryYu"
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.cornerLayout.bannerText = "HurryYu"
     }
 }
