d8826845810e69c723112c9e93c5ea5267cabc58
HurryYu <cqbbyzh@gmail.com>
2020-12-29 12:46:45 +0800

    完善示例代码,新增位置设置 与 显示隐藏的调用示例

--- app/src/main/java/com/hurryyu/cornerlayout/sample/MainActivity.kt
+++ app/src/main/java/com/hurryyu/cornerlayout/sample/MainActivity.kt
@@ -2,6 +2,7 @@ package com.hurryyu.cornerlayout.sample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.hurryyu.cornerlayout.BannerPosition
 import com.hurryyu.cornerlayout.sample.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -14,5 +15,20 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.cornerLayout.bannerText = "HurryYu"
+        binding.btnTopLeft.setOnClickListener {
+            binding.cornerLayout.bannerPosition = BannerPosition.TOP_LEFT
+        }
+        binding.btnTopRight.setOnClickListener {
+            binding.cornerLayout.bannerPosition = BannerPosition.TOP_RIGHT
+        }
+        binding.btnBottomLeft.setOnClickListener {
+            binding.cornerLayout.bannerPosition = BannerPosition.BOTTOM_LEFT
+        }
+        binding.btnBottomRight.setOnClickListener {
+            binding.cornerLayout.bannerPosition = BannerPosition.BOTTOM_RIGHT
+        }
+        binding.btnShowOrHide.setOnClickListener {
+            binding.cornerLayout.display = !binding.cornerLayout.display
+        }
     }
 }
